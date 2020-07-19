1e1c134d20dcb46d01f8c63b0dc720986930cd52
Arkesh Jaiswal <aj.xddx@gmail.com>
2020-07-19 20:22:56 +0530

    allwed host

--- freelancer/freelancer/settings.py
+++ freelancer/freelancer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rr6%57mn-zt%4^!ijv563i&1igf+pqnovi=8n+ng%uc$pt6bx('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dj-proj.herokuapp.com']
 
 
 # Application definition
